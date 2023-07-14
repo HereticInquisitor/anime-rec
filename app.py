06524c8e67760dac0cf3ebe08e0878f5b2e658ba
HereticInquisitor <ayushraj7353@gmail.com>
2023-07-14 22:20:35 +0530

    fu

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import bz2
+
+st.title('Anime Recommender System')
+
+movie_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies = pd.DataFrame(movie_dict)
+
+
+similarity = pickle.load(bz2.open('similarity.pkl','rb'))
+
+def recommend(movie):
+    m_index = movies[movies['name'] == movie].index[0]
+    distances = similarity[m_index]
+    movies_list= sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:11]
+    recommended_movies=[]
+    for i in movies_list:
+        # print(movies.iloc[i[0]].name)
+        # print(movie['name'][movies.iloc[i[0]]])
+        recommended_movies.append(movies.iloc[i[0]])
+
+    return recommended_movies
+
+option = st.selectbox(
+'Enter the name of an Anime',
+movies['name'].values
+)
+
+if st.button('Recommend'):
+    recommendations=recommend(option)
+    for i in recommendations:
+        st.write(i)
